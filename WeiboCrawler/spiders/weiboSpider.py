9ea23dee859358265440dda202d23843d2097ead
KongWiki <kongwiki@163.com>
2019-05-29 23:34:03 +0800

    微博搜索爬虫基础班完成

--- WeiboCrawler/spiders/weiboSpider.py
+++ WeiboCrawler/spiders/weiboSpider.py
@@ -8,112 +8,28 @@ import re
 import time
 from lxml import etree
 
-from scrapy import Spider
-from scrapy.crawler import  CrawlerProcess
+from scrapy.crawler import CrawlerProcess
 from scrapy.selector import Selector
 from scrapy.http import Request
 from scrapy.utils.project import get_project_settings
+from scrapy_redis.spiders import RedisSpider
 
 from WeiboCrawler.items import TweetsItem, InformationItem, RelationshipsItem, CommentItem
 from WeiboCrawler.spiders.utils import time_fix, emoji_re, extract_comment_content, extract_weibo_content
 
 
-class WeibospiderSpider(Spider):
+class WeibospiderSpider(RedisSpider):
     name = 'weiboSpider'
     # allowed_domains = ['weibo.cn']
     start_urls = 'https://weibo.cn/'
+    redis_key = "weiboSpider:start_urls"
 
-    def start_requests(self):
-        start_uids = [
-            # '537591235',  # 老中医健康养生
-            # '6002839365',  # 爱问医生-外科
-            # '5979495016', # 许超医生
-            # '1962092795', # 健康小知识库
-            # 'dxymummy' # 丁香妈妈
-            # '3120152103'
+    custom_settings = {
+        'CONCURRENT_REQUESTS': 16,
+        "DOWNLOAD_DELAY": 0.1,
+    }
 
-        ]
-        for uid in start_uids:
-            yield Request(url="https://weibo.cn/%s/info" % uid, callback=self.parseInformation)
-
-    def parseInformation(self, response):
-        """ 抓取个人信息 """
-        information_item = InformationItem()
-        information_item['crawl_time'] = int(time.time())
-        selector = Selector(response)
-        information_item['_id'] = re.findall('(\d+)/info', response.url)[0]
-        text1 = ";".join(selector.xpath('body/div[@class="c"]//text()').extract())  # 获取标签里的所有text()
-        nick_name = re.findall('昵称;?[：:]?(.*?);', text1)
-        gender = re.findall('性别;?[：:]?(.*?);', text1)
-        place = re.findall('地区;?[：:]?(.*?);', text1)
-        briefIntroduction = re.findall('简介;?[：:]?(.*?);', text1)
-        birthday = re.findall('生日;?[：:]?(.*?);', text1)
-        sex_orientation = re.findall('性取向;?[：:]?(.*?);', text1)
-        sentiment = re.findall('感情状况;?[：:]?(.*?);', text1)
-        vip_level = re.findall('会员等级;?[：:]?(.*?);', text1)
-        authentication = re.findall('认证;?[：:]?(.*?);', text1)
-        labels = re.findall('标签;?[：:]?(.*?)更多>>', text1)
-        if nick_name and nick_name[0]:
-            information_item["nick_name"] = nick_name[0].replace(u"\xa0", "")
-        if gender and gender[0]:
-            information_item["gender"] = gender[0].replace(u"\xa0", "")
-        if place and place[0]:
-            place = place[0].replace(u"\xa0", "").split(" ")
-            information_item["province"] = place[0]
-            if len(place) > 1:
-                information_item["city"] = place[1]
-        if briefIntroduction and briefIntroduction[0]:
-            information_item["brief_introduction"] = briefIntroduction[0].replace(u"\xa0", "")
-        if birthday and birthday[0]:
-            information_item['birthday'] = birthday[0]
-        if sex_orientation and sex_orientation[0]:
-            if sex_orientation[0].replace(u"\xa0", "") == gender[0]:
-                information_item["sex_orientation"] = "同性恋"
-            else:
-                information_item["sex_orientation"] = "异性恋"
-        if sentiment and sentiment[0]:
-            information_item["sentiment"] = sentiment[0].replace(u"\xa0", "")
-        if vip_level and vip_level[0]:
-            information_item["vip_level"] = vip_level[0].replace(u"\xa0", "")
-        if authentication and authentication[0]:
-            information_item["authentication"] = authentication[0].replace(u"\xa0", "")
-        if labels and labels[0]:
-            information_item["labels"] = labels[0].replace(u"\xa0", ",").replace(';', '').strip(',')
-        request_meta = response.meta
-        request_meta['item'] = information_item
-        yield Request(self.start_urls + '/u/{}'.format(information_item['_id']),
-                      callback=self.parseFurtherInformation,
-                      meta=request_meta, dont_filter=True, priority=1)
-
-    def parseFurtherInformation(self, response):
-        text = response.text
-        information_item = response.meta['item']
-        tweets_num = re.findall('微博\[(\d+)\]', text)
-        if tweets_num:
-            information_item['tweets_num'] = int(tweets_num[0])
-        follows_num = re.findall('关注\[(\d+)\]', text)
-        if follows_num:
-            information_item['follows_num'] = int(follows_num[0])
-        fans_num = re.findall('粉丝\[(\d+)\]', text)
-        if fans_num:
-            information_item['fans_num'] = int(fans_num[0])
-        yield information_item
-
-        # 获取该用户微博
-        yield Request(url=self.start_urls + '/{}/profile?page=1'.format(information_item['_id']),
-                      callback=self.parseTweet,
-                      priority=1)
-
-        # 获取关注列表
-        yield Request(url=self.start_urls + '/{}/follow?page=1'.format(information_item['_id']),
-                      callback=self.parseFollow,
-                      dont_filter=True)
-        # 获取粉丝列表
-        yield Request(url=self.start_urls + '/{}/fans?page=1'.format(information_item['_id']),
-                      callback=self.parseFans,
-                      dont_filter=True)
-
-    def parseTweet(self, response):
+    def parse(self, response):
         if response.url.endswith('page=1'):
             # 如果是第1页，一次性获取后面的所有页
             all_page = re.search(r'/>&nbsp;1/(\d+)页</div>', response.text)
@@ -122,10 +38,13 @@ class WeibospiderSpider(Spider):
                 all_page = int(all_page)
                 for page_num in range(2, all_page + 1):
                     page_url = response.url.replace('page=1', 'page={}'.format(page_num))
-                    yield Request(page_url, self.parseTweet, dont_filter=True, meta=response.meta)
+                    yield Request(page_url, self.parse, dont_filter=True, meta=response.meta)
         """
         解析本页的数据
         """
+        """
+                解析本页的数据
+                """
         tree_node = etree.HTML(response.body)
         tweet_nodes = tree_node.xpath('//div[@class="c" and @id]')
         for tweet_node in tweet_nodes:
@@ -176,7 +95,7 @@ class WeibospiderSpider(Spider):
                 all_content_link = tweet_node.xpath('.//a[text()="全文" and contains(@href,"ckAll=1")]')
                 if all_content_link:
                     all_content_url = self.start_urls + all_content_link[0].xpath('./@href')[0]
-                    yield Request(all_content_url, callback=self.parseAllContent, meta={'item': tweet_item},
+                    yield Request(all_content_url, callback=self.parse_all_content, meta={'item': tweet_item},
                                   priority=1)
 
                 else:
@@ -186,14 +105,13 @@ class WeibospiderSpider(Spider):
                         tweet_item['location'] = tweet_node.xpath('.//span[@class="ctt"]/a[last()]/text()')[0]
                     yield tweet_item
 
-                # 抓取该微博的评论信息
-                comment_url = self.start_urls + '/comment/' + tweet_item['weibo_url'].split('/')[-1] + '?page=1'
-                yield Request(url=comment_url, callback=self.parseComment, meta={'weibo_url': tweet_item['weibo_url']})
+                yield Request(url="https://weibo.cn/{}/info".format(tweet_item['user_id']),
+                              callback=self.parse_information, priority=2)
 
             except Exception as e:
                 self.logger.error(e)
 
-    def parseAllContent(self, response):
+    def parse_all_content(self, response):
         # 有阅读全文的情况，获取全文
         tree_node = etree.HTML(response.body)
         tweet_item = response.meta['item']
@@ -204,83 +122,68 @@ class WeibospiderSpider(Spider):
             tweet_item['location'] = content_node.xpath('.//span[@class="ctt"]/a[last()]/text()')[0]
         yield tweet_item
 
-    def parseFollow(self, response):
-        """
-        抓取关注列表
-        """
-        # 如果是第1页，一次性获取后面的所有页
-        if response.url.endswith('page=1'):
-            all_page = re.search(r'/>&nbsp;1/(\d+)页</div>', response.text)
-            if all_page:
-                all_page = all_page.group(1)
-                all_page = int(all_page)
-                for page_num in range(2, all_page + 1):
-                    page_url = response.url.replace('page=1', 'page={}'.format(page_num))
-                    yield Request(page_url, self.parseFollow, dont_filter=True, meta=response.meta)
+    def parse_information(self, response):
+        """ 抓取个人信息 """
+        information_item = InformationItem()
+        information_item['crawl_time'] = int(time.time())
         selector = Selector(response)
-        urls = selector.xpath('//a[text()="关注他" or text()="关注她" or text()="取消关注"]/@href').extract()
-        uids = re.findall('uid=(\d+)', ";".join(urls), re.S)
-        ID = re.findall('(\d+)/follow', response.url)[0]
-        for uid in uids:
-            relationships_item = RelationshipsItem()
-            relationships_item['crawl_time'] = int(time.time())
-            relationships_item["fan_id"] = ID
-            relationships_item["followed_id"] = uid
-            relationships_item["_id"] = ID + '-' + uid
-            yield relationships_item
+        information_item['_id'] = re.findall('(\d+)/info', response.url)[0]
+        text1 = ";".join(selector.xpath('body/div[@class="c"]//text()').extract())  # 获取标签里的所有text()
+        nick_name = re.findall('昵称;?[：:]?(.*?);', text1)
+        gender = re.findall('性别;?[：:]?(.*?);', text1)
+        place = re.findall('地区;?[：:]?(.*?);', text1)
+        briefIntroduction = re.findall('简介;?[：:]?(.*?);', text1)
+        birthday = re.findall('生日;?[：:]?(.*?);', text1)
+        sex_orientation = re.findall('性取向;?[：:]?(.*?);', text1)
+        sentiment = re.findall('感情状况;?[：:]?(.*?);', text1)
+        vip_level = re.findall('会员等级;?[：:]?(.*?);', text1)
+        authentication = re.findall('认证;?[：:]?(.*?);', text1)
+        labels = re.findall('标签;?[：:]?(.*?)更多>>', text1)
+        if nick_name and nick_name[0]:
+            information_item["nick_name"] = nick_name[0].replace(u"\xa0", "")
+        if gender and gender[0]:
+            information_item["gender"] = gender[0].replace(u"\xa0", "")
+        if place and place[0]:
+            place = place[0].replace(u"\xa0", "").split(" ")
+            information_item["province"] = place[0]
+            if len(place) > 1:
+                information_item["city"] = place[1]
+        if briefIntroduction and briefIntroduction[0]:
+            information_item["brief_introduction"] = briefIntroduction[0].replace(u"\xa0", "")
+        if birthday and birthday[0]:
+            information_item['birthday'] = birthday[0]
+        if sex_orientation and sex_orientation[0]:
+            if sex_orientation[0].replace(u"\xa0", "") == gender[0]:
+                information_item["sex_orientation"] = "同性恋"
+            else:
+                information_item["sex_orientation"] = "异性恋"
+        if sentiment and sentiment[0]:
+            information_item["sentiment"] = sentiment[0].replace(u"\xa0", "")
+        if vip_level and vip_level[0]:
+            information_item["vip_level"] = vip_level[0].replace(u"\xa0", "")
+        if authentication and authentication[0]:
+            information_item["authentication"] = authentication[0].replace(u"\xa0", "")
+        if labels and labels[0]:
+            information_item["labels"] = labels[0].replace(u"\xa0", ",").replace(';', '').strip(',')
+        request_meta = response.meta
+        request_meta['item'] = information_item
+        yield Request(self.base_url + '/u/{}'.format(information_item['_id']),
+                      callback=self.parse_further_information,
+                      meta=request_meta, dont_filter=True, priority=1)
 
-    def parseFans(self, response):
-        """
-        抓取粉丝列表
-        """
-        # 如果是第1页，一次性获取后面的所有页
-        if response.url.endswith('page=1'):
-            all_page = re.search(r'/>&nbsp;1/(\d+)页</div>', response.text)
-            if all_page:
-                all_page = all_page.group(1)
-                all_page = int(all_page)
-                for page_num in range(2, all_page + 1):
-                    page_url = response.url.replace('page=1', 'page={}'.format(page_num))
-                    yield Request(page_url, self.parseFans, dont_filter=True, meta=response.meta)
-        selector = Selector(response)
-        urls = selector.xpath('//a[text()="关注他" or text()="关注她" or text()="移除"]/@href').extract()
-        uids = re.findall('uid=(\d+)', ";".join(urls), re.S)
-        ID = re.findall('(\d+)/fans', response.url)[0]
-        for uid in uids:
-            relationships_item = RelationshipsItem()
-            relationships_item['crawl_time'] = int(time.time())
-            relationships_item["fan_id"] = uid
-            relationships_item["followed_id"] = ID
-            relationships_item["_id"] = uid + '-' + ID
-            yield relationships_item
-
-    def parseComment(self, response):
-        # 如果是第1页，一次性获取后面的所有页
-        if response.url.endswith('page=1'):
-            all_page = re.search(r'/>&nbsp;1/(\d+)页</div>', response.text)
-            if all_page:
-                all_page = all_page.group(1)
-                all_page = int(all_page)
-                for page_num in range(2, all_page + 1):
-                    page_url = response.url.replace('page=1', 'page={}'.format(page_num))
-                    yield Request(page_url, self.parseComment, dont_filter=True, meta=response.meta)
-        tree_node = etree.HTML(response.body)
-        comment_nodes = tree_node.xpath('//div[@class="c" and contains(@id,"C_")]')
-        for comment_node in comment_nodes:
-            comment_user_url = comment_node.xpath('.//a[contains(@href,"/u/")]/@href')
-            if not comment_user_url:
-                continue
-            comment_item = CommentItem()
-            comment_item['crawl_time'] = int(time.time())
-            comment_item['weibo_url'] = response.meta['weibo_url']
-            comment_item['comment_user_id'] = re.search(r'/u/(\d+)', comment_user_url[0]).group(1)
-            comment_item['content'] = extract_comment_content(etree.tostring(comment_node, encoding='unicode'))
-            comment_item['_id'] = comment_node.xpath('./@id')[0]
-            created_at_info = comment_node.xpath('.//span[@class="ct"]/text()')[0]
-            like_num = comment_node.xpath('.//a[contains(text(),"赞[")]/text()')[-1]
-            comment_item['like_num'] = int(re.search('\d+', like_num).group())
-            comment_item['created_at'] = time_fix(created_at_info.split('\xa0')[0])
-            yield comment_item
+    def parse_further_information(self, response):
+        text = response.text
+        information_item = response.meta['item']
+        tweets_num = re.findall('微博\[(\d+)\]', text)
+        if tweets_num:
+            information_item['tweets_num'] = int(tweets_num[0])
+        follows_num = re.findall('关注\[(\d+)\]', text)
+        if follows_num:
+            information_item['follows_num'] = int(follows_num[0])
+        fans_num = re.findall('粉丝\[(\d+)\]', text)
+        if fans_num:
+            information_item['fans_num'] = int(fans_num[0])
+        yield information_item
 
 
 if __name__ == "__main__":
